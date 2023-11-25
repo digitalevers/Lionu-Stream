61448e75ec25d7c0061defc7f10e291422370c4e
yongbaolinux <yongbaolinux@gmail.com>
2023-11-25 14:18:45 +0800

    fix

--- src/main/scala/testJDBC.scala
+++ src/main/scala/testJDBC.scala
@@ -1,3 +1,4 @@
+import java.util.Properties
 
 
 object testJDBC {
@@ -13,15 +14,8 @@ object testJDBC {
 //        println(ex.getMessage)
 //      }
 //    }
-    val map = Map('a'->1)
-    // 使用ClassLoader加载properties配置文件生成对应的输入流
-    val in = getClass.getClassLoader().getResourceAsStream("application.properties")
 
-    // 使用properties对象加载输入流
-    //prop.load(in)
-    //redisUtil.connect(prop.getProperty("redis.server"))
-    //val deviceExistInRedis = redisUtil.get("5-imei").getOrElse(null)
 
-    //print(deviceExistInRedis)
+
   }
 }
